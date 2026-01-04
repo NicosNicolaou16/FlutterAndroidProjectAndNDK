a93d9ee784097fbb6654d8b81ca9708f3442a567
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-05 01:05:27 +0200

    update CMake

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "4.1.1"
+            version = "4.1.2"
         }
     }
 }
